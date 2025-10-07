1460e47eb1c352b983cb721c9c291d959209c8f7
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2025-10-07 14:58:48 +0300

    a while loop to let the user guess again. , Change the for loop so that you keep the previous correct letters

--- Hangman Game/Hangman Game.py
+++ Hangman Game/Hangman Game.py
@@ -1,25 +1,38 @@
 import random
+from sys import displayhook
+
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-
-guess = input("Guess a letter: ").lower()
 placeholder = ""
-for letter in chosen_word:
+word_length = len(chosen_word)
+for position in range(word_length):
     placeholder += "_"
-
 print(placeholder)
 
-display = ""
+# a while loop to let the user guess again.
+game_over = False
+matched_before = []
 
-for letter in chosen_word:
-    if letter == guess:
-        display += guess
-    else:
-        display += "_"
+while game_over == False:
+    guess = input("Guess a letter: ").lower()
+    display = ""
 
-print(display)
+    for letter in chosen_word:
 
+        if letter == guess:
+            display += letter
+            matched_before.append(letter)
 
+        elif  letter in matched_before:
+            display += letter
+
+        else:
+            display += "_"
+
+    print(display)
+    if "_" not in display:
+        game_over = True
+        print("You won")
