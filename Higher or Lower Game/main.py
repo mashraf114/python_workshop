b8f749082776be336d0fdcf8f3dd3a762bf6ee9b
mashraf114 <mohamed.ashraf.shaban11@gmail.com>
2025-10-09 08:17:35 +0300

    Feature: Looping

--- Higher or Lower Game/main.py
+++ Higher or Lower Game/main.py
@@ -22,23 +22,46 @@ f_description = first_choice['description']
 f_country = first_choice['country']
 
 second_choice = (choice(data))
+if second_choice == first_choice:
+    second_choice = (choice(data))
+
 s_name = second_choice['name']
 s_followers= second_choice['follower_count']
 s_description = second_choice['description']
 s_country = second_choice['country']
 
 
-def compare():
+def compare(score):
     print(f"Compare A: {f_name}, {f_description},{f_country}")
     print(vs)
     print(f"Against B: {s_name}, {s_description},{s_country}")
     player_choice = input("Who has the most followers? 'A' or 'B': ").lower()
     if f_followers > s_followers and player_choice == 'a':
         print(f"Correct! {f_name} has more followers")
+        score +=1
+        print(f"Your score is {score}")
     elif f_followers < s_followers and player_choice == 'b':
         print(f"Correct! {s_name} has more followers")
+        score += 1
+        print(f"Your score is {score}")
     else:
         print(f"Incorrect! {player_choice} doesn't has more followers!")
-
+        score -= 1
+        print(f"Your score is {score}")
 print(logo)
-compare()
+score = 0
+should_continue = True
+while should_continue:
+    compare(score)
+    choice = input("continue?").lower()
+    print(choice)
+    if choice == "no" or "n":
+        should_continue = True
+        print("Good bye")
+    else:
+        print("compare")
+
+        compare()
+
+
+
